9d43fc446a5c63702a99f001f1deeb9def8313c4
Andy Zhang <zhangnd16@berkeley.edu>
2025-03-07 16:36:18 -0800

    GameController, ItemController and ItemService

--- src/main/java/com/laioffer/twitch/AppConfig.java
+++ src/main/java/com/laioffer/twitch/AppConfig.java
@@ -1,15 +1,12 @@
 package com.laioffer.twitch;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 public class AppConfig {
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- src/main/java/com/laioffer/twitch/DevelopmentTester.java
+++ src/main/java/com/laioffer/twitch/DevelopmentTester.java
@@ -1,6 +1,5 @@
 package com.laioffer.twitch;
 
-
 import com.laioffer.twitch.external.TwitchService;
 import com.laioffer.twitch.external.model.Clip;
 import com.laioffer.twitch.external.model.Game;
@@ -12,24 +11,18 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-
 import java.util.List;
-
 
 @Component
 public class DevelopmentTester implements ApplicationRunner {
 
-
     private static final Logger logger = LoggerFactory.getLogger(DevelopmentTester.class);
 
-
     private final TwitchService twitchService;
-
 
     public DevelopmentTester(TwitchService twitchService) {
         this.twitchService = twitchService;
     }
-
 
     @Override
     public void run(ApplicationArguments args) {
@@ -38,7 +31,6 @@ public class DevelopmentTester implements ApplicationRunner {
         List<Video> videos = twitchService.getVideos("21779", 2);
         List<Clip> clips = twitchService.getClips("21779", 2);
         List<Stream> streams = twitchService.getStreams(List.of("18122", "21779", "33214"), 10);
-
 
         logger.info(games.toString());
         logger.info(topGames.toString());
