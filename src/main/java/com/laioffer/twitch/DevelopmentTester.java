78f51a1a93abb6c66253a464638137151d00fa56
Andy Zhang <zhangnd16@berkeley.edu>
2025-04-14 01:01:52 -0700

    Caching, unit testing

--- src/main/java/com/laioffer/twitch/DevelopmentTester.java
+++ src/main/java/com/laioffer/twitch/DevelopmentTester.java
@@ -1,44 +1,28 @@
 package com.laioffer.twitch;
 
 
-import com.laioffer.twitch.db.FavoriteRecordRepository;
-import com.laioffer.twitch.db.ItemRepository;
-import com.laioffer.twitch.db.UserRepository;
-import com.laioffer.twitch.db.entity.FavoriteRecordEntity;
-import com.laioffer.twitch.db.entity.ItemEntity;
-import com.laioffer.twitch.db.entity.UserEntity;
-import com.laioffer.twitch.model.ItemType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.laioffer.twitch.user.UserService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-
-import java.time.Instant;
-import java.util.List;
 
 
 @Component
 public class DevelopmentTester implements ApplicationRunner {
 
 
-    private static final Logger logger = LoggerFactory.getLogger(DevelopmentTester.class);
+    private final UserService userService;
 
 
-    private final UserRepository userRepository;
-
-
-    public DevelopmentTester(
-            UserRepository userRepository
-    ) {
-        this.userRepository = userRepository;
+    public DevelopmentTester(UserService userService) {
+        this.userService = userService;
     }
+
+
 
 
     @Override
     public void run(ApplicationArguments args) {
-        UserEntity newUser = new UserEntity(null, "user0", "Foo", "Bar", "password");
-        userRepository.save(newUser);
+        userService.register("default", "123456", "John", "Smith");
     }
 }
